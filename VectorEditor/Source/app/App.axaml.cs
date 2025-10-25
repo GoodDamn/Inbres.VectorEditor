55c1710d75da11208e2f8e8c70a77e6c00807c57
GoodDamn <grigorydum@gmail.com>
2025-10-25 12:29:00 +0300

    implement 'core' project

--- VectorEditor/Source/app/App.axaml.cs
+++ VectorEditor/Source/app/App.axaml.cs
@@ -4,15 +4,14 @@ using Avalonia.Markup.Xaml;
 
 namespace VectorEditor;
 
-public partial class App : Application
-{
-    public override void Initialize()
-    {
+public partial class App
+: Application {
+    
+    public override void Initialize() {
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
-    {
+    public override void OnFrameworkInitializationCompleted() {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow();
--- VectorEditor/Source/app/MainWindow.axaml.cs
+++ VectorEditor/Source/app/MainWindow.axaml.cs
@@ -1,9 +1,10 @@
+using System;
 using Avalonia.Controls;
 
 namespace VectorEditor;
 
-public partial class MainWindow : Window
-{
+public partial class MainWindow
+: Window {
     public MainWindow()
     {
         InitializeComponent();
