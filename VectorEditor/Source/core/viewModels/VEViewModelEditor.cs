74db75c8f28667b79b8bf3c1586c2d1510bad62f
GoodDamn <grigorydum@gmail.com>
2025-10-29 08:04:07 +0300

    core:: VEViewModelEditor: create

---
+++ VectorEditor/Source/core/viewModels/VEViewModelEditor.cs
@@ -0,0 +1,27 @@
+using Avalonia;
+using core.render;
+
+namespace core.viewModels;
+
+public class VEViewModelEditor {
+    
+    private readonly VESkeleton2D _skeleton;
+
+    public VESkeleton2DRender skeletonRender { get; }
+
+    public VEViewModelEditor() {
+        _skeleton = new VESkeleton2D();
+        skeletonRender = new VESkeleton2DRender(
+            _skeleton
+        );
+    }
+    
+    public void addPoint(
+        Point point
+    ) {
+        _skeleton.points.AddLast(
+            point
+        );
+    }
+    
+}
