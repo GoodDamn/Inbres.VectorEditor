5edb36f659d9636f0055d72d32f59eaa0ccda612
GoodDamn <grigorydum@gmail.com>
2025-10-29 07:52:54 +0300

    core:: VEWindowMain: add points

--- VectorEditor/Source/app/window/VEWindowMain.axaml.cs
+++ VectorEditor/Source/app/window/VEWindowMain.axaml.cs
@@ -1,12 +1,27 @@
 using System;
+using Avalonia;
 using Avalonia.Controls;
+using core;
 using core.controls;
+using core.render;
 
 namespace VectorEditor;
 
 public partial class VEWindowMain
 : Window {
-    public VEWindowMain() {
-        Content = new VEControlVectorEditor();
+    public VEWindowMain()
+    {
+        var skeleton = new VESkeleton2D();
+        Content = new VEControlVectorEditor {
+            skeletonRender = new VESkeleton2DRender(
+                skeleton
+            )
+        };
+
+        for (int i = 0; i < 16; i++) {
+            skeleton.points.AddLast(
+                new Point(i * 75, i * 75)
+            );
+        }
     }
 }
