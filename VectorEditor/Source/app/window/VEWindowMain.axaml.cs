682c2cc0636f66d5ed701b8745c1e3df9c844659
GoodDamn <grigorydum@gmail.com>
2025-10-30 07:32:46 +0300

    app:: VEWindowMain: implement pointer listener

--- VectorEditor/Source/app/window/VEWindowMain.axaml.cs
+++ VectorEditor/Source/app/window/VEWindowMain.axaml.cs
@@ -10,17 +10,11 @@ namespace VectorEditor;
 
 public partial class VEWindowMain
 : Window {
-    public VEWindowMain()
-    {
+    public VEWindowMain() {
         VEViewModelEditor viewModel = new();
         Content = new VEControlVectorEditor {
-            skeletonRender = viewModel.skeletonRender
+            skeletonRender = viewModel.skeletonRender,
+            pointer = viewModel.pointer
         };
-
-        for (int i = 0; i < 16; i++) {
-            viewModel.addPoint(
-                new Point(i * 75, i * 75)
-            );
-        }
     }
 }
